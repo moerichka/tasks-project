ffcb1f7e151492d947cbbde1e073891ec19362c3
MichailI <mishika1221@mail.ru>
2023-09-08 20:44:06 +0600

    Добавлен новый slice в хранилище tasksSlice и подключен к проекту

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 
 import { counterReducer } from "./counterSlice";
+import { tasksReducer } from "./tasksSlice";
 
 const store = configureStore({
   reducer: {
     counter: counterReducer,
+    tasks: tasksReducer
   },
 });
 
---
+++ src/store/tasksSlice.js
@@ -0,0 +1,11 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const tasksSlice = createSlice({
+  name: "tasks",
+  initialState: {
+    tasks: [],
+  },
+  reducers: {},
+});
+
+export const tasksReducer = tasksSlice.reducer;
